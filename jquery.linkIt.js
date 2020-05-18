25594528267c87b201b6de98499103103b0fff86
j-otokoyama <otokoyama@pacbell.net>
2020-05-17 17:18:36 -0700

    Updating

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -1,1 +1,12 @@
-// Test start linkIt plugin
+/*//////////////////////////
++   name:       LinkIt
++   author:     Joey Nelson
++   version:    0.1.0
++   license:    MIT
+*///////////////////////////
+
+(function($) {
+    $.fn.linkIt = function() {
+        alert(this.html());
+    }
+}(jQuery));
