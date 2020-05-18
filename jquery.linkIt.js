f3f43ec9ff84a9a1b6e96d699adf5c787d2cffdc
j-otokoyama <otokoyama@pacbell.net>
2020-05-17 22:13:26 -0700

    Adding updated plugin files and manifest

--- jquery.linkIt.js
+++ jquery.linkIt.js
@@ -6,7 +6,27 @@
 *///////////////////////////
 
 (function($) {
-    $.fn.linkIt = function() {
-        alert(this.html());
+    $.fn.linkIt = function(options) {
+        // Default Settings
+        var settings = $.extend({
+            href: null,
+            text: null,
+            target: "_self"
+        }, options);
+
+        // Validation
+        if (settings.href == null) {
+            console.log("You need an href option for LinkIt to work");
+            return this;
+        }
+
+        return this.each(function() {
+            var object = $(this);
+
+            if (settings.text == null) {
+                settings.text = object.text();
+            }
+            object.wrap('<a href="'+settings.href+'" target="'+settings.target+'"></a>').text(settings.text);
+        });
     }
 }(jQuery));
